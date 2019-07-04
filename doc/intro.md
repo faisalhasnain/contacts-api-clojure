e04d3440e35cfeaa606cee5a20ba7cd5c7832798
Faisal Hasnain <faisalhasnain90@gmail.com>
2019-07-04 23:10:20 +0500

    initialize clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to contacts-api-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/contacts_api_clojure/core.clj
@@ -0,0 +1,7 @@
+(ns contacts-api-clojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
