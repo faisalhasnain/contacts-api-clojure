0dc782ab89c66c9c1967dfe864d5812a5eea91cb
Faisal Hasnain <faisalhasnain90@gmail.com>
2019-07-05 16:51:03 +0500

    add http routes for getting list and single record

--- src/contacts_api_clojure/core.clj
+++ src/contacts_api_clojure/core.clj
@@ -1,7 +1,29 @@
 (ns contacts-api-clojure.core
-  (:gen-class))
+  (:require [org.httpkit.server :refer [run-server]]
+            [compojure.core :refer :all]
+            [compojure.coercions :refer :all]
+            [compojure.route :as route]
+            [clojure.data.json :as json]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def contacts [{:id 1 :name "Faisal" :age 28}
+               {:id 2 :name "Sana" :age 23}])
+
+(defn get-contacts []
+  contacts)
+
+(defn get-contact [id]
+  (first (filter #(= (:id %) id) contacts)))
+
+(defn render-response [resp]
+  {:status  200
+   :headers {"Content-Type" "text/json"}
+   :body    (json/write-str resp)})
+
+(defroutes app-routes
+  (GET "/" [] (render-response (get-contacts)))
+  (GET "/:id" [id :<< as-int] (render-response (get-contact id)))
+  (route/not-found "Error, page not found!"))
+
+(defn -main [& args]
+  (run-server #'app-routes {:port 8000})
+  (println "Web server listening on port: 8000"))
