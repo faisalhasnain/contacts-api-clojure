eea9d3399482174175fbe21ff4c76a2a2c0000d6
Faisal Hasnain <faisalhasnain90@gmail.com>
2019-07-28 09:43:16 +0500

    migrate from compojure to reitit for routing

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.7.1"]
-                 [compojure "1.6.1"]
+                 [metosin/reitit-ring "0.3.9"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [ring/ring-json "0.4.0"]]
   :main ^:skip-aot contacts-api-clojure.core
--- src/contacts_api_clojure/core.clj
+++ src/contacts_api_clojure/core.clj
@@ -1,36 +1,33 @@
 (ns contacts-api-clojure.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer :all]
-            [compojure.coercions :refer :all]
-            [compojure.route :as route]
+            [reitit.ring :as ring]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.util.response :refer [response]]))
 
-(def contacts (atom [{:id 1 :name "Faisal" :twitter "faisalhasnainz"}
-                     {:id 2 :name "Paul" :twitter "paulg"}]))
+(def contacts (atom [{:id "1" :name "Faisal" :twitter "faisalhasnainz"}
+                     {:id "2" :name "Paul" :twitter "paulg"}]))
 
-(defn get-contacts []
-  @contacts)
+(defn get-contacts [request]
+  (response @contacts))
 
-(defn get-contact [id]
-  (first (filter #(= (:id %) id) @contacts)))
+(defn get-contact [{{id :id} :path-params}]
+  (response (first (filter #(= (:id %) id) @contacts))))
 
-(defn add-contact [contact]
+(defn add-contact [{contact :body}]
   (swap! contacts conj contact)
-  contact)
+  (response contact))
 
-(defn remove-contact [id]
-  (let [contact (get-contact id)]
+(defn remove-contact [{{id :id} :path-params}]
+  (let [contact (first (filter #(= (:id %) id) @contacts))]
     (swap! contacts (fn [arr] (remove #(= (:id %) id) arr)))
-    contact))
+    (response contact)))
 
-(defroutes app-routes
-  (GET "/" [] (response (get-contacts)))
-  (GET "/:id" [id :<< as-int] (response (get-contact id)))
-  (POST "/" req (response (add-contact (:body req))))
-  (DELETE "/:id" [id :<< as-int] (response (remove-contact id)))
-  (route/not-found "Error, page not found!"))
+(def router
+  (ring/ring-handler
+   (ring/router
+    [["/" {:get get-contacts :post add-contact}]
+     ["/:id" {:get get-contact :delete remove-contact}]])))
 
 (defn -main [& args]
-  (run-jetty (->  #'app-routes (wrap-json-body) (wrap-json-response)) {:port 8000})
+  (run-jetty (->  #'router (wrap-json-body) (wrap-json-response)) {:port 8000})
   (println "Web server listening on port: 8000"))
